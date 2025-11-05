5f440436f9127d1e84ef13a451d6b04c372586f1
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-04 21:29:41 -0300

    feat: create Junior class

--- src/entities/Junior.java
+++ src/entities/Junior.java
@@ -1,4 +1,23 @@
 package entities;
 
-public class Junior {
+public class Junior extends Employee{
+
+    private static Double salesPercentageBonus ;
+
+    public Junior(String name, Double baseSalary) {
+        super(name, baseSalary);
+    }
+
+    public static void setSalesPercentageBonus(Double salesPercentageBonus) {
+        Junior.salesPercentageBonus = salesPercentageBonus;
+    }
+
+    public Double getSalesPercentageBonus() {
+        return salesPercentageBonus;
+    }
+
+    @Override
+    public Double totalPayment() {
+        return getBaseSalary() + (getSalesValue() * salesPercentageBonus);
+    }
 }
