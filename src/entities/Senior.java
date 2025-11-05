c4b07f44d93cd0b28e14552d55fb99b1d5652726
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-04 21:29:57 -0300

    feat: create Senior class

--- src/entities/Senior.java
+++ src/entities/Senior.java
@@ -1,4 +1,45 @@
 package entities;
 
-public class Senior {
+public class Senior extends Employee{
+    private static Double salesPercentageBonus ;
+    private static Double goalPercentageBonus ;
+    private double salesGoal ;
+
+
+    public Senior(String name, double baseSalary, double salesGoal) {
+        super(name, baseSalary);
+        this.salesGoal = salesGoal;
+    }
+
+    public static Double getSalesPercentageBonus() {
+        return salesPercentageBonus;
+    }
+
+    public static void setSalesPercentageBonus(Double salesPercentageBonus) {
+        Senior.salesPercentageBonus = salesPercentageBonus;
+    }
+
+    public static Double getGoalPercentageBonus() {
+        return goalPercentageBonus;
+    }
+
+    public static void setGoalPercentageBonus(Double goalPercentageBonus) {
+        Senior.goalPercentageBonus = goalPercentageBonus;
+    }
+
+    public double getSalesGoal() {
+        return salesGoal;
+    }
+
+    public void setSalesGoal(double salesGoal) {
+        this.salesGoal = salesGoal;
+    }
+
+    @Override
+    public Double totalPayment() {
+        if(salesGoal <= getSalesValue()){
+            return getBaseSalary() + (getSalesValue() * salesPercentageBonus) + (getBaseSalary() * goalPercentageBonus);
+        }
+        return getBaseSalary() + (getSalesValue() * salesPercentageBonus);
+    }
 }
