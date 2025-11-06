1c8b97fda0d4c95916db66687ddeb80dc146c450
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-05 21:25:32 -0300

    create : SalesEmployee abstract class

--- src/entities/sales/SalesEmployee.java
+++ src/entities/sales/SalesEmployee.java
@@ -1,4 +1,25 @@
 package entities.sales;
 
-public class SalesEmployee {
+import entities.Employee;
+
+public abstract class SalesEmployee extends Employee {
+    private double monthSales;
+
+    public SalesEmployee() {
+        super();
+    }
+
+    public SalesEmployee(String name, Double baseSalary, double monthSales) {
+        super(name, baseSalary);
+        this.monthSales = monthSales;
+    }
+
+    public double getMonthSales() {
+        return monthSales;
+    }
+
+    public void setMonthSales(double monthSales) {
+        this.monthSales = monthSales;
+    }
+
 }
