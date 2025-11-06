2b019df40428b091abca50e8732cb1287abc3f05
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-05 21:36:19 -0300

    create: abstract dev class

--- src/entities/dev/DevEmployee.java
+++ src/entities/dev/DevEmployee.java
@@ -1,4 +1,24 @@
 package entities.dev;
 
-public class DevEmployee {
+import entities.Employee;
+
+public abstract class DevEmployee extends Employee {
+    private int projectsMade;
+
+    public DevEmployee() {
+        super();
+    }
+
+    public DevEmployee(String name, double baseSalary, int projectsMade) {
+        super(name, baseSalary);
+        this.projectsMade = projectsMade;
+    }
+
+    public int getProjectsMade() {
+        return projectsMade;
+    }
+
+    public void setProjectsMade(int projectsMade) {
+        this.projectsMade = projectsMade;
+    }
 }
