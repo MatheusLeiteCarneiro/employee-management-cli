bc2bb52183e8a00dbfc6192d6432ff37408b0a70
MatheusLeiteCarneiro <matheus.leite.carneiro@gmail.com>
2025-11-05 21:23:32 -0300

    create : HrEmployee class

--- src/entities/HrEmployee.java
+++ src/entities/HrEmployee.java
@@ -1,4 +1,16 @@
 package entities;
 
-public class HrEmployee {
+public class HrEmployee extends Employee{
+    public HrEmployee() {
+        super();
+    }
+
+    public HrEmployee(String name, Double baseSalary) {
+        super(name, baseSalary);
+    }
+
+    @Override
+    public Double totalPayment() {
+        return getBaseSalary();
+    }
 }
